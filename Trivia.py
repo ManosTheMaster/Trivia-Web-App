81ca92c5083f08d485a24fd681ef60196c3ef2be
jimfilippou <jimfilippou@hotmail.gr>
2016-12-27 02:30:48 +0200

    Fixed Postgres  Tables , Added Leaderboards , Added front end to leaderboards

--- Trivia.py
+++ Trivia.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mitsos123@localhost/user_db'
+app.debug = False
+db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    score = db.Column(db.Integer, unique=False)
+
+    def __init__(self, username, email, score):
+        self.username = username
+        self.email = email
+        self.score = score
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 # HomePage
 @app.route('/')
@@ -21,7 +36,16 @@ def technology():
 # Register User
 @app.route('/post_user', methods=['POST'])
 def post_user():
-    return 1
+    user = User(request.form['username'], request.form['email'], 0)
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/leaderboards')
+def leaderboards():
+    list = User.query.all()
+    return render_template('leaderboards.html', list=list)
 
 if __name__ == '__main__':
     app.run()
